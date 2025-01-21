4a83b8e78959836ea52f7d80fc70b4bf32478c80
kasoltani <kassoltani@gmail.com>
2025-01-21 00:36:25 -0800

    Firebase admin backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,46 @@
+// Impoorting required modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require('firebase-admin');
 
+// Initialize express
 const app = express();
 
-//Middleware
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-//test route
+// Firebase Admin SDK setup
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://chaptre-b7fcb-default-rtdb.firebaseio.com",
+});
+
+// Test route
 app.get('/', (req, res) => {
     res.send('Hello! Backend is running!');
 });
 
-//start server
+// Token verification route
+app.post('/verify-token', async (req, res) => {
+    const token = req.body.token;
+
+    try{
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        res.json({
+            success: true,
+            uid: decodedToken.uid});
+    }catch(error){
+        res.status(401).json({
+            success: false,
+            message: "Invalid Token"
+        });
+    }
+});
+
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
