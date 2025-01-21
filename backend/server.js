b1fcb3c2ef7ae1446c39a468fc46285628ca0620
kasoltani <kassoltani@gmail.com>
2025-01-20 23:18:48 -0800

    Backend and frontend initial setup and basic backend server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//test route
+app.get('/', (req, res) => {
+    res.send('Hello! Backend is running!');
+});
+
+//start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
