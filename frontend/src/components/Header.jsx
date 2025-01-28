35195d229e7d3a98702824b8cc23f0961af891de
Yali <yaligoldstein@Yalis-MBP.hsd1.ca.comcast.net>
2025-01-28 15:33:36 -0800

    did login page style and header style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../index.css'
+import '../index.css';
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/profile/1">Profile</Link></li>
-          <li><Link to="/Login">Login</Link></li>
+    <header className="header">
+
+      <Link to="/" className="nav-item">Chaptre</Link>
+
+      <nav className="nav-container">
+        <ul className="nav-list">
+          <li><Link to="/write" className="nav-item">Write</Link></li>
+          <li><Link to="/library" className="nav-item">Library</Link></li>
+          <li><Link to="/profile/1" className="nav-item">Profile</Link></li>
+          <li><Link to="/login" className="nav-item login-btn">Login</Link></li>
         </ul>
       </nav>
     </header>
