d7b436485e7f7699536a1028aca0fb5ad5b35da9
Yali <yaligoldstein@Yalis-MBP.hsd1.ca.comcast.net>
2025-02-05 13:06:44 -0800

    added basic text editor

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,7 +10,8 @@ function Header() {
 
       <nav className="nav-container">
         <ul className="nav-list">
-          <li><Link to="/write" className="nav-item">Write</Link></li>
+          {/* <li><Link to="/write" className="nav-item">Write</Link></li> */}
+          <li><Link to="/editBook" className="nav-item">Write</Link></li>
           <li><Link to="/profile/1" className="nav-item">Profile</Link></li>
           <li><Link to="/login" className="nav-item login-btn">Login</Link></li>
         </ul>
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -3,14 +3,28 @@ import TextEditor from '../components/TextEditor';
 
 function EditBook() {
   const editorRef = useRef(null);
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState(""); 
+
+  const handleContentChange = (newContent) => {
+    setContent(newContent); 
+  };
+
+  useEffect(() => {
+
+  }, []);
 
   return (
-    <div>
-      <h2>Chapter Name should go here</h2>
-      <TextEditor ref={editorRef} value={content} />
+    <div className="text-editor-container">
+      <h2>Chapter Name</h2>
+      <TextEditor 
+        ref={editorRef}
+        value={content}        
+        onChange={handleContentChange} 
+        placeholder="Start typing here..." 
+      />
     </div>
   );
+  
 }
 
 export default EditBook;
