b122fe9d26a37d97aac285c91ef93775158607cb
Yali <yaligoldstein@Yalis-MBP.hsd1.ca.comcast.net>
2025-02-05 10:22:42 -0800

    added css for home page and reading view

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom';
 
 function BookCard({ book }) {
   return (
-    <div className="book-card">
+    <Link to={`/book/${book.bookId}`} className="book-card">
       <img src={book.coverImage} alt={"Image Not Found"} />
       <h3>{book.title}</h3>
       <p>By {book.author}</p>
-      <Link to={`/book/${book.bookId}`}>Read Book</Link>
-    </div>
+    </Link>
   );
 }
 
