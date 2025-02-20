cc40c7a8977018351af331047ebb129fd92f165d
Nathan Sauton <nsauton@ucsc.edu>
2025-02-19 16:12:13 -0800

    fixed searching repeating queries

--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import BookGrid from "../components/BookGrid"; // Your grid component to display the books
 import { searchBooks } from "../services/api"; // Function to fetch books from Firestore
@@ -8,21 +8,16 @@ function SearchResults() {
   const location = useLocation(); // Get current URL for search query
   const searchParams = new URLSearchParams(location.search);
   const searchQuery = searchParams.get("q"); // Get title/author query
-  const genreFilter = searchParams.getAll("genre"); // Get genre filter
+  const genreFilter = useMemo(() => searchParams.getAll("genre"), [location.search]); //get filtered genres
+
 
   useEffect(() => {
-    console.log("searchQuery:", searchQuery);
-    console.log("genreFilter:", genreFilter);
-
     const loadBooks = async () => {
-      if (!searchQuery && genreFilter.length === 0) {
-        return;
-      } 
+      if (!searchQuery && genreFilter.length === 0) return; 
 
       try {
-        const response = await searchBooks(searchQuery, genreFilter); // Fetch books based on query
-        console.log("Books response:", response);
-        setBooks(response); // Set books for display
+        const response = await searchBooks(searchQuery, genreFilter); 
+        setBooks(response);
       } catch (error) {
         alert("FAILED TO LOAD SEARCH RESULTS");
       }
