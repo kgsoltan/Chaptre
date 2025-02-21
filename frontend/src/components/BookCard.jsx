e10f243009acb0cd674c600c01d3e4101a5b6511
neelwalse <117557589+nwalse@users.noreply.github.com>
2025-02-21 15:29:33 -0800

    chapter reordering

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import '../EditBook.css';
+import '../BookCard.css';
 
 function BookCard({ book, showEditLink }) {
   const maxVisibleTags = 2;
--- frontend/src/components/BookGrid.jsx
+++ frontend/src/components/BookGrid.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import BookCard from './BookCard';
-import '../EditBook.css';
+import '../BookCard.css';
 
 
 function BookGrid({ books, showEditLink }) {
